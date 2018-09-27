a52a424df77aa14ce5ef96a8bff7f372f7ae66ce
Joshua Park <wmpark@knowre.com>
2018-09-28 08:42:22 +0900

    Initial Commit

---
+++ ValidJSON/ValidJSON.h
@@ -0,0 +1,19 @@
+//
+//  ValidJSON.h
+//  ValidJSON
+//
+//  Created by Joshua Park on 28/09/2018.
+//  Copyright © 2018 Knowre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ValidJSON.
+FOUNDATION_EXPORT double ValidJSONVersionNumber;
+
+//! Project version string for ValidJSON.
+FOUNDATION_EXPORT const unsigned char ValidJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ValidJSON/PublicHeader.h>
+
+
